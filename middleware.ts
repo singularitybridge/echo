6220001fb0de64850fc64b76a16d7b74c01ae53b
avi.o <aviosipov@gmail.com>
2025-12-21 08:30:54 +0200

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -59,7 +59,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - model-comparison-* (static comparison pages)
      */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico|model-comparison-).*)',
   ],
 };
